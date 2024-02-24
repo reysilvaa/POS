f0da8575789a8e75a83a5caa16bd4a0782fcd869
reysilvaa <reynaldsilva123@gmail.com>
2024-02-24 17:45:43 +0700

    fix routing error

---
+++ resources/views/index.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Home</title>
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
+
+        header {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 10px;
+        }
+
+        nav {
+            background-color: #444;
+            color: #fff;
+            padding: 10px;
+        }
+
+        nav ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: space-around;
+        }
+
+        nav a {
+            text-decoration: none;
+            color: #fff;
+            font-weight: bold;
+        }
+
+        section {
+            padding: 20px;
+        }
+
+        footer {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 10px;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Selamat Datang di POS</h1>
+    </header>
+
+    <section>
+        <p>Gambar</p>
+    </section>
+
+    <footer>
+        <p>Rey</p>
+    </footer>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,21 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SalesController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'home']);
+
+Route::prefix('products')->group(function () {
+    Route::get('/category/food-beverage', [ProductController::class, 'foodBeverage']);
+    Route::get('/category/beauty-health', [ProductController::class, 'beautyHealth']);
+    Route::get('/category/home-care', [ProductController::class, 'homeCare']);
+    Route::get('/category/baby_kid', [ProductController::class, 'babyKid']);
 });
+
+Route::get('/user/{id}/name/{name}', [UserController::class, 'user']);
+
+Route::get('/sales', [SalesController::class, 'sales']);
